b4affa4150e40bb40c0f1fc34db84e5560bea18a
Bertrand Tan <bertrandTWM@gmail.com>
2022-03-10 09:51:53 +0000

    add code in files types: .html,.py,.js remove code in files types: .html,.py,.js

--- content.js
+++ content.js
@@ -1,3 +1,3 @@
 console.log("epsvy")
 console.log("kxugr")
-console.log("ywmlr")
+console.log("draah")
--- content.py
+++ content.py
@@ -1,4 +1,4 @@
 print("dsxys")
 print("amitl")
 print("pbabb")
-print("ueghx")
+print("sfaei")
